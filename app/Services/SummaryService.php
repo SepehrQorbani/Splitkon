6378094a576fa5dbf9b15d367c9018e4689a33ae
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-04-17 01:58:23 +0330

    Fixed summary data issue

--- app/Services/SummaryService.php
+++ app/Services/SummaryService.php
@@ -9,17 +9,17 @@ class SummaryService
     {
         $group->load([
             'expenses' => function ($query) {
-                $query->orderByDesc('date')->take(3);
+                $query->with('spender')->orderByDesc('date')->take(3);
             },
             'repays' => function ($query) {
-                $query->orderByDesc('date')->take(3);
+                $query->with(['from', 'to'])->orderByDesc('date')->take(3);
             },
             'members'
         ]);
-        $group->loadCount(['expenses', 'repays']);
-
-        $totalExpenses = $group->expenses->sum('amount');
-        $totalRepays = $group->repays->sum('amount');
+        $group->loadCount(['expenses', 'repays'])
+            ->loadSum(['expenses', 'repays'], 'amount');
+        // $totalExpenses = $group->expenses()->sum('amount');
+        // $totalRepays = $group->repays()->sum('amount');
         $pendingBalances = (new BalanceService($group->members))->calculate(false, true);
         $totalOutstanding = 0;
         $netBalances = $group->members->map(function ($member) use (&$totalOutstanding) {
@@ -34,21 +34,47 @@ class SummaryService
         });
         $totalRatio = $group->members->sum('ratio');
 
+        // Combine and group recent activities by date
+        $recentActivities = [];
+
+        $group->expenses->each(function ($expense) use (&$recentActivities) {
+            $date = $expense->date->format('Y-m-d');
+            if (!isset($recentActivities[$date])) {
+                $recentActivities[$date] = [
+                    'date' => $date,
+                    'expenses' => [],
+                    'repays' => []
+                ];
+            }
+            $recentActivities[$date]['expenses'][] = $expense;
+        });
+
+        $group->repays->each(function ($repay) use (&$recentActivities) {
+            $date = $repay->date->format('Y-m-d');
+            if (!isset($recentActivities[$date])) {
+                $recentActivities[$date] = [
+                    'date' => $date,
+                    'expenses' => [],
+                    'repays' => []
+                ];
+            }
+            $recentActivities[$date]['repays'][] = $repay;
+        });
+        $recentActivities = collect($recentActivities)->sortByDesc('date')->values()->all();
+        // dd($recentActivities);
         return [
             'members_count' => $group->members->count(),
             'expenses_count' => $group->expenses_count,
+            'days_count' => ceil($group->date->diffInDays()),
             'repays_count' => $group->repays_count,
             'total_ratio' => $totalRatio,
-            'total_expenses' => $totalExpenses,
-            'total_repays' => $totalRepays,
+            'total_expenses' => $group->expenses_sum_amount,
+            'total_repays' => $group->repays_sum_amount,
             'total_outstanding' => $totalOutstanding,
             'balance_status' => $netBalances->every(fn($balance) => $balance['net'] === 0) ? 'تراز شده' : 'تراز نشده',
             'net_balances' => $netBalances->toArray(),
             'pending_balances' => $pendingBalances,
-            'recent_activity' => [
-                'expenses' => $group->expenses,
-                'repays' => $group->repays,
-            ],
+            'recent_activity' => $recentActivities,
         ];
     }
 
