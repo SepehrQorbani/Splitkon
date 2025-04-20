90a4f08bfba1dec4d3f233fa6c75a31cc52d31e0
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-04-20 11:03:34 +0330

    Refactor ProgressBar component for improved layout and styling

--- client/src/components/common/ProgressBar.tsx
+++ client/src/components/common/ProgressBar.tsx
@@ -11,19 +11,41 @@ interface ProgressChartProps extends AriaProgressBarProps {
 }
 function ProgressBar({ label, className, ...props }: ProgressChartProps) {
     return (
-        <AriaProgressBar className={cn("text-xs", className)} {...props}>
+        <AriaProgressBar className={cn("text-xs pe-2", className)} {...props}>
             {({ percentage, valueText }) => (
                 <>
-                    <div className="flex justify-between">
-                        <span>{valueText}</span>
-                        <Label>{label}</Label>
+                    <div className="w-full text-right mb-1.5 relative">
+                        {percentage && (
+                            <div className="-me-2">
+                                <span
+                                    className="bg-action text-action-fg px-1 rounded-sm relative z-1 shadow"
+                                    dir="ltr"
+                                    style={{
+                                        marginRight: 100 - percentage + "%",
+                                    }}
+                                >
+                                    <span className="w-3 h-3 absolute -bottom-3 right-0">
+                                        <svg
+                                            width={8}
+                                            height={8}
+                                            viewBox="0 0 12 12"
+                                            className="fill-action"
+                                        >
+                                            <path d="M0 0 L6 6 L12 0" />
+                                        </svg>
+                                    </span>
+                                    {valueText}
+                                </span>
+                            </div>
+                        )}
+                        {/* <Label>{label}</Label> */}
                     </div>
                     <div
-                        className="h-2 relative w-full rounded border border-border py-1 px-0.5 flex items-center"
+                        className="h-1.5 box-content relative w-full rounded-full border border-border p-1 flex"
                         dir="ltr"
                     >
                         <div
-                            className="h-1 rounded-full bg-action"
+                            className="rounded-full bg-action/40 border border-action"
                             style={{ width: percentage + "%" }}
                         />
                     </div>
