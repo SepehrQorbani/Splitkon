899f4a9cb99eab6136a26c1111bc745cfb84d1d6
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-05-26 16:35:00 +0330

    feat: Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
+    <link rel="icon" href="/images/icons/favicon.ico" sizes="any">
+    <link rel="icon" href="/images/icons/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/images/icons/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="/images/icons/apple-touch-icon.png">
     @viteReactRefresh
     @vite('client/src/main.tsx')
 </head>
