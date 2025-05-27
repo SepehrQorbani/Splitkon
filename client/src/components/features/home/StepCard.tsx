dcd694117839962404812a67ec110d2f75f20e5c
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-05-27 13:54:35 +0330

    feat(client)(StepCard): Improved layout

--- client/src/components/features/home/StepCard.tsx
+++ client/src/components/features/home/StepCard.tsx
@@ -1,4 +1,6 @@
 import { Card } from "@/components/common/Card";
+import { useTranslations } from "@/hooks/useTranslations";
+import { cn } from "@/utils/cn";
 import { motion } from "motion/react";
 
 interface StepCardProps {
@@ -16,61 +18,109 @@ export const StepCard: React.FC<StepCardProps> = ({
     description,
     isLast = false,
 }) => {
+    const { direction } = useTranslations();
     return (
         <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.5, delay: number * 0.1 }}
-            className="w-full relative pe-4 md:pe-0"
+            className="w-full relative"
         >
-            <Card className="h-full p-0 w-full rounded-2xl gap-0">
-                <div className="relative w-full">
-                    <div className="absolute mx-2 my-2 inset-0 -z-10- [background-size:12px_12px] [background-image:radial-gradient(var(--color-muted-subtle)_1px,transparent_1px)]"></div>
-                    <img
-                        src={`/images/${image}`}
-                        className="p-1 block h-48 w-full object-contain object-top relative"
-                        alt=""
-                    />
-                </div>
-                <div className="px-6 py-4 flex flex-col gap-4">
-                    <h3 className="text-xl font-semibold">
+            <div className="flex flex-col h-full ps-6 md:ps-0">
+                {!isLast && (
+                    <svg
+                        viewBox="0 0 4 100"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute start-0 -z-10 top-6 pt-6 h-full md:hidden"
+                        stroke="var(--color-muted-subtle)"
+                        strokeWidth="0.5"
+                    >
+                        <motion.path
+                            d="M0 0.25H4"
+                            whileInView={{
+                                pathLength: [0, 1],
+                                // pathOffset: [, 0],
+                            }}
+                            transition={{ duration: 0.1 }}
+                        />
+                        <motion.path
+                            d="M2 0V98"
+                            whileInView={{
+                                pathLength: [0, 1],
+                            }}
+                            transition={{ duration: 0.5 }}
+                        />
+                        <motion.path
+                            d="M0 97 L 2 100"
+                            whileInView={{
+                                pathLength: [0, 1],
+                            }}
+                            transition={{ delay: 0.5, duration: 0.1 }}
+                        />
+                        <motion.path
+                            d="M2 100 L 4 97"
+                            whileInView={{
+                                pathLength: [0, 1],
+                            }}
+                            transition={{ delay: 0.6, duration: 0.1 }}
+                        />
+                    </svg>
+                )}
+                <div className="flex items-center relative -ms-8 md:ms-0">
+                    <h3 className="relative text-xl font-semibold px-2 py-4 shrink-0">
                         {number}. {title}
                     </h3>
-                    <p className="text-muted-soft">{description}</p>
-                </div>
-                {!isLast && (
-                    <>
-                        <svg
-                            viewBox="0 0 19 194"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-full absolute -z-10 -end-4 top-1/2 block md:hidden stroke-muted-soft"
-                        >
-                            <path
-                                d="M18.0002 193V193C8.86042 193 1.45117 185.591 1.45117 176.451V17.5488C1.45117 8.40917 8.86033 1 18 1V1"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeDasharray="4 4"
-                            />
-                        </svg>
 
-                        <svg
-                            viewBox="0 0 58 194"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="py-8- h-2/3 absolute end-0 -translate-x-3/4 py-4 -z-10 hidden md:block stroke-muted-soft"
-                        >
-                            <path
-                                d="M57 193H52.451C39.1962 193 28.451 182.255 28.451 169V25C28.451 11.7452 17.7058 1 4.45099 1H1"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeDasharray="4 4"
-                            />
-                        </svg>
-                    </>
-                )}
-            </Card>
+                    {!isLast && (
+                        <>
+                            <svg
+                                viewBox="0 0 200 23"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={cn(
+                                    "absolute -z-10 ms-4 bottom-2 shrink-0 w-full hidden md:block",
+                                    direction === "ltr" && "-scale-x-100"
+                                )}
+                                stroke="var(--color-muted-subtle)"
+                            >
+                                <motion.path
+                                    d="M81 21.5H200"
+                                    whileInView={{
+                                        pathLength: [0, 1],
+                                        pathOffset: [1, 0],
+                                    }}
+                                    transition={{ duration: 0.5 }}
+                                />
+                                <motion.path
+                                    d="M3 11.0001C26.5 10.0001 27 15.5772 43.5001 21.5001C60.0002 27.423 68 1.50012 53.5 1.00011C41.5859 0.589281 39.975 21.501 66.5 21.5C70.5 21.4999 74 21.5001 81 21.5001M4.5 14.5001L1.01488 11.7895C0.500134 11.3891 0.500135 10.6112 1.01488 10.2108L4.5 7.50015"
+                                    whileInView={{
+                                        pathLength: [0, 1],
+                                        // pathSpacing: [0.2, 0.4],
+                                        pathOffset: [1, 0],
+                                    }}
+                                    transition={{ delay: 0.5, duration: 1 }}
+                                />
+                            </svg>
+                        </>
+                    )}
+                </div>
+                <Card className="h-full relative z-0 p-0 w-full rounded-2xl gap-0 justify-between overflow-clip">
+                    <div className="relative w-full">
+                        <img
+                            src={`/images/${image}`}
+                            className="p-1 block h-48 w-full object-contain object-top relative"
+                            alt=""
+                        />
+                    </div>
+                    <div className="relative px-6 py-4 flex flex-col gap-4 h- text-sm text-justify">
+                        <p className="text-muted-soft">{description}</p>
+                        <div className="absolute bottom-0 left-0 right-0 h-full -z-10 bg-linear-0 from-surface from-70% to-surface/0"></div>
+                    </div>
+                    <div className="absolute inset-2 -z-11 [background-size:12px_12px] [background-image:radial-gradient(var(--color-muted-subtle)_1px,transparent_1px)]"></div>
+                </Card>
+            </div>
         </motion.div>
     );
 };
