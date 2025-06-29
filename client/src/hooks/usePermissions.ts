05781c28154c05507b30915516180b81facef58f
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-06-29 17:33:01 +0330

    fix(client): Update usePermissions hook to allow optional PermissionKey parameter in can function

--- client/src/hooks/usePermissions.ts
+++ client/src/hooks/usePermissions.ts
@@ -2,13 +2,16 @@ import { usePermissionStore } from "@/store/permissions";
 import { PermissionKey } from "@/types";
 
 interface PermissionResult {
-    can: (key: PermissionKey) => boolean;
+    can: (key?: PermissionKey) => boolean;
 }
 
 export const usePermissions = (): PermissionResult => {
     const permissions = usePermissionStore((state) => state.permissions);
 
-    const can = (key: PermissionKey): boolean => permissions[key] || false;
+    const can = (key?: PermissionKey): boolean => {
+        if (key === undefined) return false;
+        return permissions[key] || false;
+    };
 
     return { can };
 };
