c49e6d7a3e43939e94f4ce37525c73963216caf0
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-08-04 12:29:00 +0330

    fix(client): Correct Instagram and Twitter links

--- client/src/constants/links.ts
+++ client/src/constants/links.ts
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = {
     github: "https://github.com/sepehrqorbani/splitkon",
-    instagram: "https://instagram.com/splitkon",
+    instagram: "https://instagram.com/splitkonapp",
     telegram: "https://t.me/splitkonapp",
-    twitter: "https://twitter.com/splitkon",
+    twitter: "https://twitter.com/splitkonapp",
     gmail: "splitkonapp@gmail.com",
 } as const;
