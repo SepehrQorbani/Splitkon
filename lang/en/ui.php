f4406458fc4d4b4b209b023b0ac42d00785fb049
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-08-04 12:43:53 +0330

    feat: Work on SEO optimization (wip)

---
+++ lang/en/ui.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'appName' => 'SplitKon',
+    'alternateName' => 'SplitKon',
+    'splitkon' => 'SplitKon',
+    'description' => 'SplitKon: Free, open-source app for seamless group expense management.',
+    'metaKeywords' => 'group expense sharing app, split bills app, shared expenses management, group trip cost sharing, open-source expense tracker, no sign-up expense sharing',
+    'feature1' => 'Easily and fairly split group expenses',
+    'feature2' => 'Smart calculation of each person\'s share',
+    'feature3' => 'No sign-up required',
+    'feature4' => 'Manage travel, dorm, and building fees',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Models\Group;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{path?}', function () {
+// Route::get('/', function (?string $path = null) {
+//     return view('home', [
+//         'layout' => 'main',
+//     ]);
+// });
+
+Route::get('/{path?}', function (?string $path = null) {
+    $token = explode('/', $path)[0];
+    if (strlen($token) === 32) {
+        $group = Group::where('view_token', $token)
+            ->orWhere('edit_token', $token)
+            ->first();
+
+        return view('app', ['group' => $group]);
+    }
+
     return view('app');
 })->where('path', '^(?!api).*');
