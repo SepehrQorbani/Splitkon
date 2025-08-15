70724ab2e121d16d68876e88d245d9bb85046a31
SepehrQorbani <sepehr.qorbani@gmail.com>
2025-08-15 11:53:14 +0330

    feat: Implement RepayResource for structured response and update RepayController methods

--- app/Http/Controllers/RepayController.php
+++ app/Http/Controllers/RepayController.php
@@ -4,40 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RepayStoreRequest;
 use App\Http\Requests\RepayUpdateRequest;
-use App\Services\RepayService;
+use App\Http\Resources\RepayResource;
 use App\Models\Repay;
+use App\Services\RepayService;
 
 class RepayController extends Controller
 {
     public function index()
     {
-        $repays = request()->attributes->get("group")
+        $repays = request()->attributes->get('group')
             ->repays()->with(['from', 'to', 'attachments'])->orderBy('date')->get();
-        return response()->json(['data' => $repays]);
+
+        return RepayResource::collection($repays);
     }
 
     public function store(RepayStoreRequest $request, RepayService $repayService)
     {
         $repay = $repayService->createRepay($request->validated());
-        return response()->json(['data' => $repay->load(['from', 'to', 'attachments'])]);
+
+        return new RepayResource($repay->load(['from', 'to', 'attachments']));
     }
 
     public function show($token, $repayId)
     {
         $group = request()->attributes->get('group');
         $repay = $group->repays()->with(['from', 'to', 'attachments'])->findOrFail($repayId);
-        return response()->json(['data' => $repay]);
+
+        return new RepayResource($repay);
     }
 
     public function update(RepayUpdateRequest $request, $token, Repay $repay, RepayService $repayService)
     {
         $repay = $repayService->updateRepay($repay, $request->validated());
-        return response()->json(['data' => $repay->load(['from', 'to', 'attachments'])]);
+
+        return new RepayResource($repay->load(['from', 'to', 'attachments']));
     }
 
     public function destroy(Repay $repay, RepayService $repayService)
     {
         $repayService->destroyRepay($repay);
+
         return response()->noContent();
     }
 }
